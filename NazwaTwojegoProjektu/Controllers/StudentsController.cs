4ac9eb57c43e3819945656aab20f33ed96108866
Antoni Dawiowicz <s27366@pjwstk.edu.pl>
2024-05-12 12:04:09 +0200

    adw

---
+++ NazwaTwojegoProjektu/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace apbdWyk8.Controllers;
+
+[ApiController]
+[Route("api/controller")]
+public class StudentsController : ControllerBase
+{
+    public StudentsController()
+    {
+        
+    }
+
+    [HttpGet]
+    public IActionResult GetStudents()
+    {
+
+        return Ok();
+    }
+}
